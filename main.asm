253cfd35240755487c9a42eaf5c5164b1bcd1c14
AYA-NASH <67653730+AYA-NASH@users.noreply.github.com>
2021-01-10 03:46:25 +0200

    First remove

--- main.asm
+++ main.asm
@@ -238,6 +238,17 @@ scan_for_null2:
             cmp BYTE PTR [edx], 0       ; Terminating null?
             jne lets_remove           ; no -> next character
             jmp next_item_to_remove2
+	    
+lets_remove:
+             mov cl , '='
+             mov [edx] , cl
+             inc edx                     ; Pointer to next string
+             jmp remove_number
+        
+remove_number2:
+            cmp BYTE PTR [edx], 0       ; Terminating null?
+            jne lets_remove           ; no -> next character
+            jmp next_item_to_remove3
 	
 	
   done:
